ecd383fd77fa2d3d9e9934376d4b63606bee8322
Leticia Albuquerque <lety.albu6@gmail.com>
2021-06-23 14:42:56 -0300

    Initial commit

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const SpecificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (SpecificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
