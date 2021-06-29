5390063d279217b95dc549982c7ab518758f1e8a
Leticia Albuquerque <lety.albu6@gmail.com>
2021-06-29 17:51:30 -0300

    feat(user): add user authentication with JWT

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -27,8 +27,14 @@ class UsersRepository implements IUsersRepository {
     await this.repository.save(user);
   }
 
-  findByEmail(email: string): Promise<User> {
-    const user = this.repository.findOne({ email });
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
 
     return user;
   }
