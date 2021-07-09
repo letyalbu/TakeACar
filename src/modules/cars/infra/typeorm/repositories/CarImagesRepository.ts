aa9432080cf8b106e00792b1595eeeeb7724836c
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-09 16:08:45 -0300

    feat(cars): add remove car images by id

--- src/modules/cars/infra/typeorm/repositories/CarImagesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarImagesRepository.ts
@@ -12,14 +12,24 @@ class CarImagesRepository implements ICarImagesRepository {
   }
 
   async create(car_id: string, image_name: string): Promise<CarImage> {
-    const carImage = this.repository.create({
+    const car_image = this.repository.create({
       car_id,
       image_name,
     });
 
-    await this.repository.save(carImage);
+    await this.repository.save(car_image);
 
-    return carImage;
+    return car_image;
+  }
+
+  async delete(car_image: CarImage): Promise<void> {
+    await this.repository.remove(car_image);
+  }
+
+  async findById(id: string): Promise<CarImage> {
+    const car_image = await this.repository.findOne(id);
+
+    return car_image;
   }
 }
 
--- src/modules/cars/repositories/ICarImagesRepository.ts
+++ src/modules/cars/repositories/ICarImagesRepository.ts
@@ -2,6 +2,8 @@ import { CarImage } from "../infra/typeorm/entities/CarImage";
 
 interface ICarImagesRepository {
   create(car_id: string, image_name: string): Promise<CarImage>;
+  delete(car_image: CarImage): Promise<void>;
+  findById(id: string): Promise<CarImage>;
 }
 
 export { ICarImagesRepository };
