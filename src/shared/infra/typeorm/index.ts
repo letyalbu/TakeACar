4d93a3dafeb5250cd2d0e6ea7fa882e0451c8ae0
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-13 16:49:10 -0300

    test(category): check the creation of a category

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,11 @@ export default async (host = "database_takeacar"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "takeacar_test"
+          : defaultOptions.database,
     })
   );
 };
