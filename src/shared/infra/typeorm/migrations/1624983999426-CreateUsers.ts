9b7275f135f26fbe6e0721697ed311b8293e4db4
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-13 15:43:01 -0300

    feat(rental): add create a new rental

--- src/shared/infra/typeorm/migrations/1624983999426-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1624983999426-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1624983999426 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
