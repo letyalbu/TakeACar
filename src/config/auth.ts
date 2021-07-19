2ac18cb5e1ec2e2cd092854cd43bdeddd4b74753
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-19 17:28:23 -0300

    feat(auth): add refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "9783ea575193a8f983a825f9c6c0db0e",
+  expires_in_token: "15m",
+  secret_refresh_token: "3d1ab41f9dbb578cbcf1b8c7515af13a",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,62 @@
+import { sign, verify } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import auth from "@config/auth";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute(token: string): Promise<string> {
+    const {
+      secret_refresh_token,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
+
+    const { sub: user_id, email } = verify(
+      token,
+      secret_refresh_token
+    ) as IPayload;
+
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token
+      );
+
+    if (!userToken) {
+      throw new AppError("Refresh token does not exist!");
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user_id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    await this.usersTokensRepository.create({
+      user_id,
+      expires_date: this.dateProvider.addDays(expires_refresh_token_days),
+      refresh_token,
+    });
+
+    return refresh_token;
+  }
+}
+
+export { RefreshTokenUseCase };
