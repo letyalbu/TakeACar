58ba0969348dfa51f8ef5fa177a54ffaea9b92fe
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-22 17:19:07 -0300

    feat(user):add template to password recovery email

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
@@ -20,8 +21,17 @@ class SendForgotPasswordMailUseCase {
     private mailProvider: IMailProvider
   ) {}
 
-  async execute(email: string): void {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
+
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
 
     if (!user) {
       throw new AppError("Email not found!");
@@ -36,10 +46,16 @@ class SendForgotPasswordMailUseCase {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
     await this.mailProvider.sendMail(
       email,
       "Password Recovery",
-      `The link to reset is ${token}`
+      variables,
+      templatePath
     );
   }
 }
