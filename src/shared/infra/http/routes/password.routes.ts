a5b9f2a16725b8b7ce4b6cab4b36a5275a545f21
Leticia Albuquerque <lety.albu6@gmail.com>
2021-07-22 18:38:32 -0300

    feat(user): add reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordsRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordsRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordsRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordsRoutes };
